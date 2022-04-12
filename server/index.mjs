123663a24649022369d65c245c7586a237b6ea3b
flguerin <florentguerin41@gmail.com>
2022-04-12 14:58:30 +0200

    installation bot et creation bot cote serveur

--- server/index.mjs
+++ server/index.mjs
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
+import RiveScript from 'rivescript';
 
 
 import {Bot} from "./model/bot.mjs";
@@ -25,6 +26,107 @@ const port = 3001
 
 app.use(bodyParser.json()) 
 app.use(bodyParser.urlencoded({ extended: true })) 
+
+
+function creer_bot() {
+	// Create the bot.
+	var script = new RiveScript();
+	script.loadDirectory('./brain').then(success_handler(script)).catch(error_handler);
+}
+
+function success_handler(script) {
+  console.log('Brain loaded!');
+  script.sortReplies();
+
+  /*// Set up the Express app.
+  var app = express();*/
+  
+  // Parse application/json inputs.
+  app.use(bodyParser.json());
+  app.set('json spaces', 4);
+  
+  // Set up routes.
+  app.post('/reply', getReply);
+  //app.get('*', showUsage);
+  
+  /*// Start listening.
+  app.listen(3001, function () {
+     console.log('Listening on http://localhost:3001');
+  });*/
+}
+  
+function error_handler(loadcount, err) {
+  console.log('Error loading batch #' + loadcount + ': ' + err + '\n');
+}
+
+// POST to /reply to get a RiveScript reply.
+function getReply(req, res) {
+	// Get data from the JSON post.
+	var username = req.body.username;
+	var message = req.body.message;
+	var vars = req.body.vars;
+  
+	// Make sure username and message are included.
+	if (typeof username === 'undefined' || typeof message === 'undefined') {
+	  return error(res, 'username and message are required keys');
+	}
+	
+	// Copy any user vars from the post into RiveScript.
+	if (typeof vars !== 'undefined') {
+	  for (var key in vars) {
+		if (vars.hasOwnProperty(key)) {
+		  script.setUservar(username, key, vars[key]);
+		}
+	  }
+	}
+  
+	// Get a reply from the bot.
+	script
+	  .reply(username, message, this)
+	  .then(function (reply) {
+		// Get all the user's vars back out of the script to include in the response.
+		vars = script.getUservars(username);
+  
+		// Send the JSON response.
+		res.json({
+		  status: 'ok',
+		  reply: reply,
+		  vars: vars,
+		});
+	  })
+	  .catch(function (err) {
+		res.json({
+		  status: 'error',
+		  error: err,
+		});
+	  });
+}
+
+// All other routes shows the usage to test the /reply route.
+function showUsage(req, res) {
+	var egPayload = {
+	  username: 'soandso',
+	  message: 'Hello script',
+	  vars: {
+		name: 'Soandso',
+	  },
+	};
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.write('Usage: curl -i \\\n');
+	res.write('   -H "Content-Type: application/json" \\\n');
+	res.write("   -X POST -d '" + JSON.stringify(egPayload) + "' \\\n");
+	res.write('   http://localhost:3001/reply');
+	res.end();
+}
+	
+// Send a JSON error to the browser.
+function error(res, message) {
+	res.json({
+	  status: 'error',
+	  message: message,
+	});
+}
+
 /*
 app.get('/v2/tasks/', (req, res)=>{
 	try{
@@ -102,10 +204,12 @@ app.post('/',(req,res)=>{
 	let cerveau = req.body.cerveau;
 
 	let newbot=Bot.create(title,cerveau);
-
+	console.log(req.body);
 	console.log(newbot);
+	creer_bot();
 	console.log('cachalot');
-
+	
+	res.status(201).send('All is OK');
 	/*taskServiceInstance
 		.addTask(theTaskToAdd) 
 		.then((returnString)=>{
